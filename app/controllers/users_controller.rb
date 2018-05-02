89920079fc0711e4139aa1545b8faa64614f4d08
ArielFeingold <feingold.ariel@gmail.com>
2018-05-02 09:56:23 -0400

    added session & static pages controller, created signup & homepage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    byebug
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
