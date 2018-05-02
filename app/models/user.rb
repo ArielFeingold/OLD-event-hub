015fa0665d888d73599e05cf4fd50ecfb251913c
ArielFeingold <feingold.ariel@gmail.com>
2018-05-02 10:50:29 -0400

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   has_and_belongs_to_many :events
   has_many :comments
 
+  validates :first_name, :last_name, :birthday, :password, :email, presence: true { message: "must be given" }
+  validates :email, uniqueness: true { message: "The email you provided is already used" }
+  validates :username, uniqueness: true { message: "The username you provided is already taken" }
+  validates :password, confirmation: true { message: "Passwords don't match" }
+
+
 end
